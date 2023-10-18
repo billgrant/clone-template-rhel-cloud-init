bc5ccff8464af79b3a41051b9672990cc7f70423
billgrant <bill.grant@hashicorp.com>
2023-10-17 22:04:54 -0400

    first commit

---
+++ variables.tf
@@ -0,0 +1,68 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+# Credentials
+
+variable "vsphere_server" {
+  type = string
+}
+
+variable "vsphere_insecure" {
+  type    = bool
+  default = false
+}
+
+# vSphere Settings
+
+variable "vsphere_datacenter" {
+  type = string
+}
+
+variable "vsphere_cluster" {
+  type = string
+}
+
+variable "vsphere_datastore" {
+  type = string
+}
+
+variable "vsphere_folder" {
+  type = string
+}
+
+variable "vsphere_network" {
+  type = string
+}
+
+variable "vsphere_template" {
+  type = string
+}
+
+# Virtual Machine Settings
+
+variable "vm_name" {
+  type = string
+}
+
+variable "vm_cpus" {
+  type = number
+}
+
+variable "vm_memory" {
+  type = number
+}
+
+variable "vm_firmware" {
+  type = string
+}
+
+variable "vm_efi_secure_boot_enabled" {
+  type = bool
+}
+
+variable "ssh_public_key" {
+  type = string
+}
+
+##################################################################################
